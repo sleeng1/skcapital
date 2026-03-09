4d3e5bb6b3291eaa4230c6b67e3459a988645a0f
sleeng1 <agafonov.sasha29@gmail.com>
2026-03-09 13:00:41 +0500

    php 2

--- send.php
+++ send.php
@@ -3,9 +3,9 @@
 $token = "8291398458:AAESy8Rfq82b1FbmFfrv0XsN4PF-YITHRWA";
 $chat_id = "873329592";
 
-$name = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['email']);
-$message = htmlspecialchars($_POST['message']);
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
 
 $text = "📩 Новая заявка с сайта\n\n".
         "👤 Имя: $name\n".
@@ -19,21 +19,13 @@ $data = [
     "text" => $text
 ];
 
-$options = [
-    "http" => [
-        "header" => "Content-type: application/x-www-form-urlencoded\r\n",
-        "method" => "POST",
-        "content" => http_build_query($data),
-    ]
-];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
+$result = curl_exec($ch);
+curl_close($ch);
 
-if ($result) {
-    echo json_encode(["success" => true]);
-} else {
-    echo json_encode(["success" => false]);
-}
-
-?>
+echo json_encode(["success" => true]);
